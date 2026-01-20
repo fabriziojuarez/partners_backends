e935072b20f7f5dd4c199a0bb2448807bec4d4d4
fabriziojuarez <fabriziojuarez79@gmail.com>
2026-01-20 15:56:48 -0500

    feat(roles): se agrego el campo herarchy

--- database/migrations/2026_01_03_162134_create_roles_table.php
+++ database/migrations/2026_01_03_162134_create_roles_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('herarchy')->unique();
             $table->string('prefix')->unique();
             $table->string('description')->nullable();
             $table->timestamps();
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,28 +15,33 @@ class RoleSeeder extends Seeder
     {
         Role::create([
             'name' => 'Barista Part Time',
+            'herarchy' => 1,
             'prefix' => 'PT',
         ]);
 
         Role::create([
             'name' => 'Barista Full Time',
+            'herarchy' => 2,
             'prefix' => 'FT',
         ]);
 
         Role::create([
             'name' => 'Barista Trainer',
+            'herarchy' => 3,
             'prefix' => 'BT',
             'description' => 'Administracion de partners FT y PT, Administracion de cursos y Evaluaciones',
         ]);
 
         Role::create([
             'name' => 'Asistente de tienda',
+            'herarchy' => 4,
             'prefix' => 'SSV',
             'description' => 'Administracion de partners y ascenderlos hasta BT'
         ]);
 
         Role::create([
             'name' => 'Gerente de tienda',
+            'herarchy' => 5,
             'prefix' => 'SM',
             'description' => 'Administracion de partners y ascenderlos hasta SSV',
         ]);
