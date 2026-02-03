7c35384f681ef18cdb138bae57d9b273bfb24a4a
fabriziojuarez <fabriziojuarez79@gmail.com>
2026-02-03 11:50:27 -0500

    fix(topicPolicyTest): correccion en test

--- tests/Unit/Policies/TopicPolicyTest.php
+++ tests/Unit/Policies/TopicPolicyTest.php
@@ -28,7 +28,7 @@ class TopicPolicyTest extends TestCase
         return $this->assertTrue($policy->create($BT_User, $BT_Course));
     }
 
-    public function test_ssv_user_no_manager_cannot_create_topic(){
+    public function test_ssv_user_no_manager_course_cannot_create_topic(){
         $SSV_Partner = $this->makePartner('SSV', 'Usuario Comun');
         $SSV_User = $SSV_Partner->user;
 
@@ -40,7 +40,7 @@ class TopicPolicyTest extends TestCase
         return $this->assertFalse($policy->create($SSV_User, $BT_Course));
     }
 
-    public function test_pt_admin_can_update_topic(){
+    public function test_pt_admin_no_manager_course_cannot_update_topic(){
         $PT_Partner = $this->makePartner('PT', 'Administrador');
         $PT_User = $PT_Partner->user;
 
@@ -52,6 +52,6 @@ class TopicPolicyTest extends TestCase
         $BT_Topic->setRelation('course', $BT_Course);
 
         $policy = new TopicPolicy();
-        return $this->assertTrue($policy->update($PT_User, $BT_Topic, $BT_Course));
+        return $this->assertFalse($policy->update($PT_User, $BT_Topic, $BT_Course));
     }
 }
